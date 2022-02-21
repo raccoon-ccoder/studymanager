8f1331b1e31da501e34f382ceaa28f09d3c100de
raccoon-ccoder <qorwjddus96@naver.com>
2022-02-21 23:08:24 +0900

    220221_노래 재생 기능 구현 commit date : 220221 comment - 음악 자동 재생 기능 구현 - 음악 멈춤, 재생 기능 구현 - 이전 음악, 다음 음악 재생 기능 구현

---
+++ js/music.js
@@ -0,0 +1,106 @@
+'use strict';
+const all_song = [
+    {
+        singer : "bensound",
+        song : "thejazzpiano",
+        path : "bensound - thejazzpiano.mp3"
+    },
+    {
+        singer : "Chiro",
+        song : "Slump",
+        path : "Chiro_ Slump.mp3"
+    },
+    {
+        singer : "DJ Quads",
+        song : "At My Front Door",
+        path : "DJ Quads - At My Front Door.mp3"
+    },
+    {
+        singer : "lukrembo",
+        song : "jazz beat music",
+        path : "lukrembo - jazz beat music.mp3"
+    },
+    {
+        singer : "No Copyright",
+        song : "Chill Lofi Music",
+        path : "No Copyright - Chill Lofi Music.mp3"
+    },
+    {
+        singer : "Ryan",
+        song : "Milk Coffee",
+        path : "Ryan_ Milk Coffee.mp3"
+    }
+];
+
+const audioPlayer = document.querySelector("#audioContainer");
+const playButton =  document.querySelector(".footer-music__player--play");
+const previousButton = document.querySelector(".footer-music__player--previous");
+const NextButton = document.querySelector(".footer-music__player--skip");
+const musicName = document.querySelector(".footer-music__name");
+const volumeChanger = document.querySelector(".footer-music__volume");
+
+let index = 0;
+let playing_song = false;
+
+audioPlayer.addEventListener("ended", isMusicOver);
+playButton.addEventListener("click", justPlay);
+previousButton.addEventListener("click", previousMusic);
+NextButton.addEventListener("click", nextMusic);
+volumeChanger.addEventListener("change", changeVolume);
+
+function loadMusic(index) {
+    audioPlayer.src = `music/${all_song[index].path}`;
+    musicName.innerText = `${all_song[index].singer} - ${all_song[index].song}`;
+    audioPlayer.load();
+}
+
+loadMusic(index);
+
+function playMusic() {
+    audioPlayer.play();
+    playing_song = true;
+    playButton.innerText = "pause";
+}
+
+function isMusicOver() {
+    nextMusic();
+}
+
+function pauseMusic() {
+    audioPlayer.pause();
+    playing_song = false;
+    playButton.innerText = "play_arrow";
+}
+
+function justPlay() {
+    if(playing_song === false) {
+        playMusic();
+    }else {
+        pauseMusic();
+    }
+}
+
+function nextMusic() {
+    if(index < all_song.length -1) {
+        index += 1;
+    }else {
+        index = 0;
+    }
+    loadMusic(index);
+    playMusic();
+}
+
+function previousMusic() {
+    if(index > 0) {
+        index -= 1;
+    }else {
+        index = all_song.length;
+    }
+    loadMusic(index);
+    playMusic();
+}
+
+function changeVolume() {
+    const volume = document.querySelector(".footer-music__volume").value;
+    audioPlayer.volume = volume / 100;
+}
