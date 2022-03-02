47ea2b42ee5e83e29a4af30bce69b088cf3fd565
raccoon-ccoder <qorwjddus96@naver.com>
2022-03-03 04:42:07 +0900

    220303_.gitignore 수정

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArZYMKpS8eG6u0xaV7CXMnZuCjTNIM744",
+  authDomain: "studymanager-jy.firebaseapp.com",
+  databaseURL: "https://studymanager-jy-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studymanager-jy",
+  storageBucket: "studymanager-jy.appspot.com",
+  messagingSenderId: "504016003422",
+  appId: "1:504016003422:web:ad40580d70516e84b26fcf"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+export { app, db };
