1149dae2bf1d2591a907ce8b6676cca86f08cb67
raccoon-ccoder <qorwjddus96@naver.com>
2022-05-08 21:08:38 +0900

    220508_create userInfo context

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,17 +1,46 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/authContext';
 import * as S from './style';
 import Logo from '../../images/logo.png';
+import { User } from 'firebase/auth';
 
 interface IUser {
+    accessToken: string,
+    auth: object,
+    displayName: string,
+    email: string,
+    emailVerified: boolean,
+    isAnonymous: boolean,
+    metadata: object,
+    phoneNumber: string | null,
     photoURL: string,
-
+    proactiveRefresh: object,
+    providerData: object,
+    providerId: string,
+    reloadListener: any,
+    reloadUserInfo: object,
+    stsTokenManager: object,
+    tenantId: any,
+    uid: string,
 };
-
 
 function Main() {
     const userInfo = useContext(AuthContext);
-    const photo = userInfo?.photoURL;
+    const [time, setTime] = useState("0");
+
+    const currentTime = () => {
+        const date = new Date();
+        const minutes = String(date.getMinutes()).padStart(2,"0");
+        const hours = String(date.getHours()).padStart(2,"0");
+        setTime(`${hours}:${minutes}`);
+    }
+ 
+    const showTime = () => {
+        setInterval(currentTime, 60000);
+    };
+
+    showTime();
+
     return (
         <S.MainInner>
             <S.Header>
@@ -20,11 +49,11 @@ function Main() {
                 </S.LogoContainer>
                 <S.InfoContainer>
                     <S.ProfileContainer>
-                        <S.ProfileImg src={photo || undefined} />
+                        <S.ProfileImg src={userInfo?.photoURL || ""} />
                     </S.ProfileContainer>
                     <S.ClockContainer>
                         <S.ClockIcon />
-                        <S.Clock />
+                        <S.Clock>{time}</S.Clock>
                     </S.ClockContainer>
                 </S.InfoContainer>
             </S.Header>
